0ff2b98529ddde1cff9dd301fe52498dbdd9f324
Brandon Smith <107975382+BSmith311@users.noreply.github.com>
2022-09-13 11:33:05 -0700

    Add files via upload

---
+++ Hmwk/Review_Concepts/RC_Q6_Search_Array_for_Value/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=rc_q6_search_array_for_value
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/rc_q6_search_array_for_value
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=rcq6searcharrayforvalue.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/rcq6searcharrayforvalue.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=rc_q6_search_array_for_value
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/rc_q6_search_array_for_value
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=rcq6searcharrayforvalue.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/rcq6searcharrayforvalue.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
