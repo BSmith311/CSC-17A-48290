4436fefb0cac864e192780d214ae985ba7974653
Brandon Smith <107975382+BSmith311@users.noreply.github.com>
2022-09-13 11:31:44 -0700

    Add files via upload

---
+++ Hmwk/Review_Concepts/RC_Q1_Sort_1D_Array_of_Chars/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=rc_q1_sort_1d_array_of_chars
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/rc_q1_sort_1d_array_of_chars
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=rcq1sort1darrayofchars.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/rcq1sort1darrayofchars.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=rc_q1_sort_1d_array_of_chars
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/rc_q1_sort_1d_array_of_chars
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=rcq1sort1darrayofchars.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/rcq1sort1darrayofchars.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ Hmwk/Review_Concepts/RC_Q3_Find_MaxMinSum_of_IntArray/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=rc_q3_find_maxminsum_of_intarray
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/rc_q3_find_maxminsum_of_intarray
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=rcq3findmaxminsumofintarray.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/rcq3findmaxminsumofintarray.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=rc_q3_find_maxminsum_of_intarray
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/rc_q3_find_maxminsum_of_intarray
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=rcq3findmaxminsumofintarray.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/rcq3findmaxminsumofintarray.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
